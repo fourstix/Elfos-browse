b74ce48fd13e256f65262c1c955b4ec23abf1f93
fourstix <fourstix@gmail.com>
2024-11-17 12:56:07 -0500

    Add build and clean batch files to source

---
+++ src/build.bat
@@ -0,0 +1,6 @@
+[Your_Path]\Asm02\asm02 -L -D1802MINIPLUS browse.asm
+[Your_Path]\Asm02\asm02 -L -D1802MINIPLUS brws_util.asm
+[Your_Path]\Asm02\asm02 -L -D1802MINIPLUS brws_keys.asm
+[Your_Path]\Asm02\asm02 -L -D1802MINIPLUS brws_file.asm
+
+[Your_Path]\Link02\link02 -e -s browse.prg brws_util.prg brws_keys.prg brws_file.prg  
